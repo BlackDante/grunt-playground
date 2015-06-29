b00b4395f67d9bff6909e6035d6d50b31d747bd2
Kamil Kiełbasa <kamil.m.kielbasa@gmail.com>
2015-06-29 22:39:44 +0200

    initial commit

---
+++ dist/js/grunt-example.js
@@ -0,0 +1,9 @@
+(function (root) {
+  root.AnotherClass = function () {};
+})(window);
+;(function (root) {
+  root.MainClass = function () {};
+})(window);
+;(function (root) {
+  root.SecondClass = function () {};
+})(window);
---
+++ gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['app/js/**/*.js'],
+        dest: 'dist/js/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', 'app/js/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    less: {
+      build: {
+        files: {
+          'dist/css/style.css': 'app/less/style.less'
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  // Loading plugins.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Reginster Tasks.
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('style', ['less']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'less']);
+};
